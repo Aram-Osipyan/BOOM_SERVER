f25b31ff4379db05139f7c7471a5af7bc03bb7cd
Aramcho <osipyan.aram2017@ya.ru>
2020-11-29 00:17:30 +0300

    mske some test methods

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -2,8 +2,20 @@ class ChatChannel < ApplicationCable::Channel
   # Вызывается, когда потребитель успешно
   # стал подписчиком этого канала
   def subscribed
-    p "subscibed to chat"
-    stream_from “chat_channel_1”
+    p "subscibed to chat ogoooooooo"
+
+  end
+
+  def follow
+    p "follow ogoooooooo"
+  end
+
+
+  def recieve(data)
+    #p "data : "
+    p data
+    #g = ActiveSupport::JSON.decode(data)
+    p data["some"]
   end
 
 end
